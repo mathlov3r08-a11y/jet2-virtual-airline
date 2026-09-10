46c5fe36def9ee99cf0d9be6c00c8a71b585e7a9
mathlov3r08-a11y <mathlov3r08@gmail.com>
2026-09-10 17:56:37 -0400

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,5 +1,9 @@
 export default {
-  async fetch(request, env) {
-    return env.ASSETS.fetch(request);
+  async fetch() {
+    return new Response("Jet2 | PTFS API online", {
+      headers: {
+        "content-type": "text/plain"
+      }
+    });
   }
 };
